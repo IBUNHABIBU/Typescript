5f22feddc65ad0840a1d27849d9d9941d97881b0
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-28 11:32:31 +0300

    Add conmponent

--- reactypescript/src/Components/PostCard.tsx
+++ reactypescript/src/Components/PostCard.tsx
@@ -1,10 +1,10 @@
 import PostList from "./PostList"
 
-const PostCard = (props) => {
+const PostCard = (props: { title: string, desc: string }) => {
   return (
     <div className="postcard">
         <h1>{ props.title} </h1>
-        
+        <p>{ props.desc } </p>
     </div>
   )
 }
