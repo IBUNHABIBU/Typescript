b1f13d65b29c45c5ac31f3c55fd40ec18efeca26
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-01 11:19:59 +0300

    Add event

--- reactypescript/src/Components/Form.tsx
+++ reactypescript/src/Components/Form.tsx
@@ -25,14 +25,14 @@ const Form = () => {
             <h1>Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae velit molestias repellendus quaerat. Nesciunt illum cupiditate commodi alias a? Eveniet quibusdam nihil totam eligendi quaerat fugit quis molestias officia autem.
             Voluptates dolore eligendi, impedit, molestiae architecto sunt blanditiis veritatis soluta maxime pariatur aspernatur iusto laboriosam hic voluptatem! Error, animi, sint est repudiandae rem ad aut autem at in, veritatis sunt!</p>
-        <button onClick={ ()=> handleDelete(1) }>delete</button>
+        <button onClick={ (e)=> handleDelete(e, 1) }>delete</button>
         </form>
 
         <form className="post">
             <h1>Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae velit molestias repellendus quaerat. Nesciunt illum cupiditate commodi alias a? Eveniet quibusdam nihil totam eligendi quaerat fugit quis molestias officia autem.
             Voluptates dolore eligendi, impedit, molestiae architecto sunt blanditiis veritatis soluta maxime pariatur aspernatur iusto laboriosam hic voluptatem! Error, animi, sint est repudiandae rem ad aut autem at in, veritatis sunt!</p>
-        <button>delete</button>
+        <button onClick={(e) => handleDelete(e, 2)}>delete</button>
         </form>
     </div>
   )
