945bf68d1b0803c7f27d2a0016405c32f4546046
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-03 17:08:19 +0300

    Add event

--- reactypescript/src/Components/User.tsx
+++ reactypescript/src/Components/User.tsx
@@ -3,8 +3,13 @@ import React, { useState } from 'react';
 const User = () => {
     const [username, setUsername] = useState('');
     const [user, setUser] = useState(null)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement> ) => {
+        setUsername(e.target.value)
+    }
   return (
     <div>
+        <h3>User</h3>
         <form action="">
             <input type="text" placeholder='Username' onChange={handleChange}/>
             <button onClick={handleClick}>Sign in</button>
