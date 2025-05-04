f9ee5c6444c3c4d8e9ccfdc66f1bb9e66d10f2b3
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-04 22:02:20 +0300

    Add event

--- reactypescript/src/App.tsx
+++ reactypescript/src/App.tsx
@@ -4,12 +4,14 @@ import Child from './children/Child'
 import Parent from './children/Parent'
 import Form from './Components/Form'
 import PostList from './Components/PostList'
+import User from './Components/User'
 
 function App() {
 
   return (
     <>
       <div>
+        <User />
         <Form />
         <Parent>
           <Child />
