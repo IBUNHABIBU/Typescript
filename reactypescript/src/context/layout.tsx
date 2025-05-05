7b83cb58c6f5cd44aca88e5298f87ccae670b5c7
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-05 23:34:31 +0300

    Add child components

--- reactypescript/src/context/layout.tsx
+++ reactypescript/src/context/layout.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { ThemeProvder } from './ThemeContext'
 
 const layout = ({children}: { children : React.ReactNode}) => {
   return (
     <div>
         <html lang='eng'>
             <body className={inter.className}>
+                <ThemeProvder>
                 <div className="container">
                     <NavBar />
                     {children}
                 </div>
+                </ThemeProvder>
+                
             </body>
         </html>
     </div>
