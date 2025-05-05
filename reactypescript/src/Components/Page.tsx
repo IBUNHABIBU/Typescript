4f998bb58d17d121a72d3083d9ec68817ed4b377
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-05 23:37:49 +0300

    Add child components

---
+++ reactypescript/src/Components/Page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <div className="useContextExample">
+            <button>changeTheme</button>
+            <button>Change font</button>
+        </div>
+    </div>
+  )
+}
+
+export default Page
